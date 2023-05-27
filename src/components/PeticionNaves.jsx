7526b6a0e00c423c4006ade8dd4d35eaf22b5c3e
unknown <geralgranadosb@gmail.com>
2023-05-27 12:32:02 -0500

    segundo commit

--- src/components/PeticionNaves.jsx
+++ src/components/PeticionNaves.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
 
 const PeticionNaves = () => {
+  
+  const [naves, setNaves] = React.useState([])
+  const [paginacion, setPaginacion] = React.useState(0)
+  
+  const listadoNaves = async(paginacion) => {
+    const traer = await fetch (`https://api.spacexdata.com/v3/rockets?limit=2&offset=${paginacion}`)
+    const respuesta = await traer.json()
+  
+      setNaves(respuesta)
+      console.log(respuesta)   
+  }
+
+    const siguiente = () =>{
+      setPaginacion(paginacion+2)
+      listadoNaves(paginacion+2)
+    }
+    
+    const atrás = () =>{
+      setPaginacion(paginacion-2)
+      listadoNaves(paginacion-2)
+    }
+
   return (
-    <div>PETICION  DE NAVES ESPACIALES</div>
+    <div className="body">
+      <h1 > PETICIÓN DE NAVES ESPACIALES</h1><hr/>
+      <th><button onClick={listadoNaves}>Ver Naves Espaciales</button> </th>
+      <th><button onClick={siguiente}>Siguiente</button></th>
+      <th><button onClick={atrás}>Atrás</button><br /></th>
+      <hr />
+    
+      {
+        naves.map(({rocket_name: name,flickr_images: image})=> (
+          <div className="contenedor">
+          <h4>{name}</h4>
+          <img src={image} alt="" />
+          </div>
+        )
+        )
+       
+      }
+</div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import PeticionNaves from './components/PeticionNaves';  
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PeticionNaves/>
   </React.StrictMode>
 );
 
